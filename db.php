751f85321e782d70e7b562097f72000e4bfeb384
chaitawat namkrathok <tiwanamkrathok@gmail.com>
2025-06-15 16:43:39 +0700

    'webmove1.1.5'

--- db.php
+++ db.php
@@ -1,21 +1,18 @@
 <?php
 
 $db_config = [
-    'servername' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'dbname' => 'wepmove'
+    'servername' => getenv("MYSQLHOST"),
+    'username' => getenv("MYSQLUSER"),
+    'password' => getenv("MYSQLPASSWORD"),
+    'dbname'   => getenv("MYSQLDATABASE")
 ];
-
 
 $conn = new mysqli($db_config['servername'], $db_config['username'], $db_config['password'], $db_config['dbname']);
 
-
 if ($conn->connect_error) {
-    error_log("Connection failed: " . $conn->connect_error); // บันทึกข้อผิดพลาด
+    error_log("Connection failed: " . $conn->connect_error);
     die("การเชื่อมต่อล้มเหลว");
 }
-
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 ?>
